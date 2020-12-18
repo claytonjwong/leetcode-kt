e5606722b0b6bf0112eb5d5ed500d59e83f433f7
Clayton Wong <claytonjwong@gmail.com>
2020-12-18 14:46:33 -0700

    334. Increasing Triplet Subsequence (concise)

--- 334_increasing_triplet_subsequence.kt
+++ 334_increasing_triplet_subsequence.kt
@@ -2,22 +2,21 @@
  * 334. Increasing Triplet Subsequence
  *
  * Q: https://leetcode.com/problems/increasing-triplet-subsequence/
- * A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Track-Two-Minimums
+ * A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Construct-a-b-c
  */
 
 class Solution {
     fun increasingTriplet(A: IntArray): Boolean {
-        var a: Long = (6e9).toLong()
-        var b: Long = (6e9).toLong()
+        var INF: Long = (6e9).toLong()
+        var a: Long = INF
+        var b: Long = INF
+        var c: Long = INF
         var ok = false
         A.forEach { x ->
-            if (x <= a)
-                a = x.toLong()
-            else if (x <= b)
-                b = x.toLong()
-            else
-                ok = true
+            if      (x <= a) a = x.toLong()
+            else if (x <= b) b = x.toLong()
+            else if (c <= c) c = x.toLong()
         }
-        return ok
+        return c < INF
     }
 }
