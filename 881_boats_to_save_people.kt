e8e18728225348257a16a80dea5fc83cf1bc1061
Clayton Wong <claytonjwong@gmail.com>
2021-01-15 10:22:20 -0700

    881. Boats to Save People

--- 881_boats_to_save_people.kt
+++ 881_boats_to_save_people.kt
@@ -5,6 +5,24 @@
  * A: https://leetcode.com/problems/boats-to-save-people/discuss/1014992/Kt-Js-Py3-Cpp-Greedily-Consume-Boats
  */
 
+// concise
+class Solution {
+    fun numRescueBoats(A: IntArray, K: Int): Int {
+        var cnt = 0
+        A.sort()
+        var N = A.size
+        var i = 0
+        var j = N - 1
+        while (i <= j) {
+            if (A[i] + A[j] <= K)
+                ++i                // 🙂 i-th person sometimes fits in the boat ⛵️
+            ++cnt; --j             // 🙂 j-th person always    fits in the boat ⛵️
+        }
+        return cnt                 // 🎯 minimum boat count via 💰 greedy consumption
+    }
+}
+
+// verbose
 class Solution {
     fun numRescueBoats(A: IntArray, K: Int): Int {
         var cnt = 0
