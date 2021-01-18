852dad640557d73231107e2ba5de2bf8eeb2ff0f
Clayton Wong <claytonjwong@gmail.com>
2021-01-18 08:34:23 -0700

    1679. Max Number of K-Sum Pairs (more concise)

--- 1679_ksum_pairs.kt
+++ 1679_ksum_pairs.kt
@@ -4,22 +4,18 @@
  * Q: https://leetcode.com/problems/max-number-of-k-sum-pairs/
  * A: https://leetcode.com/problems/max-number-of-k-sum-pairs/discuss/962118/Kt-Js-Py3-Cpp-Map
  */
-
 class Solution {
-    fun maxOperations(A: IntArray, k: Int): Int {
-        var pairs = 0
+    fun maxOperations(A: IntArray, T: Int): Int {
+        var cnt = 0
         var m = mutableMapOf<Int, Int>()
         A.forEach { x ->
-            var y = k - x
-            if (!m.contains(y)) {
-                m[x] = if (m.contains(x)) m[x]!!.plus(1) else 1
+            var y = T - x
+            if (m.contains(y) && 0 < m[y]!!) {
+                m[y] = -1 + m.getOrDefault(y, 0); ++cnt
             } else {
-                ++pairs
-                m[y] = m[y]!!.minus(1)
-                if (m[y] == 0)
-                    m.remove(y)
+                m[x] =  1 + m.getOrDefault(x, 0)
             }
         }
-        return pairs
+        return cnt
     }
 }
